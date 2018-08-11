f4983d8015ba81fd0dcf5ecfb63efee84469cd37
Carlos <clopezalcaraz@gmail.com>
2018-08-11 12:30:03 +0200

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+## Aplicacion de clima curso node
+
+Este es el ejercicio del curso
+
+Recuerden instalar los paquetes de node
+
+```
+npm install
+```
+
+### Ejemplo :
+```
+ node app -d "Barcelona"
+```
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000; // si no existe por defecto es 3000
+
+app.use(express.static(__dirname + '/public'));
+//EXPRESS HBS
+hbs.registerPartials(__dirname + '/views/parciales')
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home', { nombre: 'CarLos lOpez' });
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+app.listen(3000, () => console.log(`Escuchando peticiones en el puerto ${port}`));
